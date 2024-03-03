85aa842e98014bc537a4bb1745b9c06dee0a3fb6
zeaarafat <alzea.arafat@icloud.com>
2024-03-03 07:27:59 +0700

    Initial Commit

---
+++ waifuapp/waifuappApp.swift
@@ -0,0 +1,17 @@
+//
+//  waifuappApp.swift
+//  waifuapp
+//
+//  Created by Alzea Arafat on 03/03/24.
+//
+
+import SwiftUI
+
+@main
+struct waifuappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
